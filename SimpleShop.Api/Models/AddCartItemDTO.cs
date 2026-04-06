42cd4c5d4bb77a6259692a36a6eb3b269541e0ec
EmilEmilHub2 <10464750@ucn.dk>
2026-04-06 20:53:39 +0200

    fix: fjernet lokal fra ci workflow

--- SimpleShop.Api/Models/AddCartItemDTO.cs
+++ SimpleShop.Api/Models/AddCartItemDTO.cs
@@ -2,7 +2,7 @@ namespace SimpleShop.Api.Models;
 
 public class AddCartItemDto
 {
-    public int UserId { get; set; }
-    public int ProductId { get; set; }
-    public int Quantity { get; set; }
+    public int? UserId { get; set; }
+    public int? ProductId { get; set; }
+    public int? Quantity { get; set; }
 }
